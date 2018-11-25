2cee1c62dd453108a27735b9b71defb1e296a9b0
cvonsteg <cvonstegmann@gmail.com>
2018-11-25 21:41:18 +0000

    updated docstrings and method names

--- dfClass.py
+++ dfClass.py
@@ -1,24 +1,32 @@
 import pandas as pd
 
 class Dataset:
+    """Simple methods for doing common operations on any pandas data frame"""
     def __init__(self, df):
         self.df = df
 
-    def Dims(self):
+    def dimensions(self):
+        """Prints dimensions of dataframe"""
         print('Data Frame Format: \n Rows: ', 
         self.df.shape[0],
         '\n Columns: ', self.df.shape[1])
 
-    def Comp(self):
+    def completeness(self):
+        """Checks for proportion of missing values in each column"""
         x = self.df.isnull().sum()
         y = (self.df.isnull().count() - self.df.isnull().sum())/self.df.isnull().count()*100
         compdf = pd.concat([x,y], axis = 1, keys = ['Missing','Completeness'])
         print(compdf)
 
-    def Desc(self):
-        print(self.df.describe())
+    #def Desc(self):
+    #    print(self.df.describe())
 
-    def LowCols(self):
+    def lower_cols(self):
+        """Casts column names to lower case"""
+        self.df.columns = self.df.columns.str.lower()
+
+    def upper_cols(self):
+        """Casts column names to upper case"""
         self.df.columns = self.df.columns.str.lower()
 
     def index_checker(self, col):
